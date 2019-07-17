6de6cf4eeb31c66f40bb9a667b334bf46a9bacd2
Misha <misha@paperspace.com>
2019-07-16 20:44:04 -0400

    2 workers 1 master

--- scripts/tf_cnn_benchmarks/benchmark_cnn.py
+++ scripts/tf_cnn_benchmarks/benchmark_cnn.py
@@ -608,10 +608,10 @@ flags.DEFINE_integer('allreduce_merge_scope', 1,
                      'parallel ops.')
 
 # Distributed training parameters.
-job_name = job_name()
-if job_name == 'master':
-    job_name = 'worker'
-flags.DEFINE_enum('job_name', job_name, ('ps', 'worker', 'controller', ''),
+# job_name = job_name()
+# if job_name == 'master':
+#     job_name = 'worker'
+flags.DEFINE_enum('job_name', job_name(), ('ps', 'worker', 'controller', ''),
                   'One of "ps", "worker", "controller", "".  Empty for local '
                   'training')
 flags.DEFINE_string('ps_hosts', ps_hosts(), 'Comma-separated list of target hosts')
